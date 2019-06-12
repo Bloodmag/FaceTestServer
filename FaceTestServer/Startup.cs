5cd8778e81df63ce20cbaffad2c6038ae43918a2
Данила Золотопупов <bloodmagv@gmail.com>
2019-06-12 16:40:50 +0300

    Add project files.

---
+++ FaceTestServer/Startup.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FaceTestServer
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.Map("/sync",Sync);
+
+            
+            
+
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Page not found");
+            });
+        }
+
+        private static void Sync(IApplicationBuilder app)
+        {
+            app.Run(async context =>
+            {
+                Dictionary<string, string> dic = new Dictionary<string, string>();
+                dic.Add("currenttime", DateTime.Now.Ticks.ToString());
+                dic.Add("version", "100");
+                dic.Add("lastupdate", new DateTime(2001, 6, 6).Ticks.ToString());
+
+                await context.Response.WriteAsync(XML.ParamsToXml(dic).ToString());
+            });
+        }
+
+    }
+}
---
+++ FaceTestServer/Utils.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace FaceTestServer
+{
+    static class XML
+    {
+        public static XDocument ParamsToXml(Dictionary<string, string> param)
+        {
+            var xml = new XDocument(new XElement("params"));
+            foreach (string p in param.Keys)
+            {
+                xml.Root.Add(new XElement(p.ToLower(), param[p].ToLower()));
+            }
+            return xml;
+        }
+
+        public static Dictionary<string, string> XmlToParams(XDocument doc)
+        {
+            Dictionary<string, string> dict = new Dictionary<string, string>();
+            foreach (XElement p in doc.Element("params").Elements())
+            {
+                dict.Add(p.Name.ToString(), p.Value);
+            }
+            return dict;
+        }
+    }
+}
